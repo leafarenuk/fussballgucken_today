53aa97eaf7d9aedc598978e7b84bba2f5ff25e25
leafarenuk <rafael.kune@me.com>
2019-08-13 18:59:50 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,22 +1,16 @@
 import requests
 from bs4 import BeautifulSoup
-import collections
 import re
 import scraperwiki
 
 url = 'https://www.fussballgucken.info/fussball-heute'
 
-content = []
-Match = collections.namedtuple('Match', 'date league kick_off home away coverages')
-Duell = collections.namedtuple('Duell', 'home away')
-
-
 try:
     page_response = requests.get(url, timeout=5)
-    
+
     if page_response.status_code == 200:
         page_content = BeautifulSoup(page_response.content, 'lxml')
-        
+
         standings_table = page_content.find('div', attrs={'class': 'match-table'})
         date = [gl.get_text() for gl in standings_table.select('.row.date')]
         kick_off = [tm.get_text() for tm in standings_table.select('.time')]
@@ -25,13 +19,14 @@ try:
         away_team = [tr.get_text() for tr in standings_table.select('.match .col-xs-5.only-top-padding.text-left')]
         coverages = [cv.get_text() for cv in standings_table.select('.coverages')]
         games = [gm.get_text() for gm in standings_table.select('.row.game')]
-        
+
         for x, game in enumerate(games):
             # content.append(Match(date, leagues[x].text.strip(), kick_off[x], home_team[x], away_team[x], (coverages[x])))
 
-            scraperwiki.sql.save(data={"date": date, "leagues": leagues[x].text.strip(),
-                                                             "kickoff": kick_off[x], "home_team": home_team[x],
-                                                             "away_team": away_team[x], "coverages": coverages[x]})
+            content = {"leagues": leagues[x].text.strip(), "kickoff": kick_off[x], "home_team": home_team[x],
+                       "away_team": away_team[x], "coverages": coverages[x]}
+
+            scraperwiki.sql.save(['home_team'], data=content)
 
     else:
         print(page_response.status_code)
@@ -39,3 +34,4 @@ try:
 except requests.Timeout as e:
     print('Timeout occurred for requested page: ' + url)
     print(str(e))
+
