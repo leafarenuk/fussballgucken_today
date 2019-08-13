370f1997aa02fdb2dee40b85df9c50b52cdcb1d7
Rafael Kune <kune@hornetsecurity.com>
2019-08-13 16:56:49 +0200

    changed save cal

--- scraper.py
+++ scraper.py
@@ -29,7 +29,7 @@ try:
         for x, game in enumerate(games):
             # content.append(Match(date, leagues[x].text.strip(), kick_off[x], home_team[x], away_team[x], (coverages[x])))
 
-            scraperwiki.sql.save(unique_keys=['name'], data={"date": date, "leagues": leagues[x].text.strip(),
+            scraperwiki.sql.save(data={"date": date, "leagues": leagues[x].text.strip(),
                                                              "kickoff": kick_off[x], "home_team": home_team[x],
                                                              "away_team": away_team[x], "coverages": coverages[x]})
 
