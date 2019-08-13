ef8dc9bc2f15bd9192bbcbebcb67f7d4a93d218a
leafarenuk <rafael.kune@me.com>
2019-08-13 19:07:19 +0200

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -4,6 +4,7 @@ import re
 import scraperwiki
 
 url = 'https://www.fussballgucken.info/fussball-heute'
+covs = []
 
 try:
     page_response = requests.get(url, timeout=5)
@@ -17,14 +18,18 @@ try:
         leagues = standings_table.findAll('a', href=re.compile("wettbewerb"))
         home_team = [tl.get_text() for tl in standings_table.select('.match .col-xs-5.only-top-padding.text-right')]
         away_team = [tr.get_text() for tr in standings_table.select('.match .col-xs-5.only-top-padding.text-left')]
+
         coverages = [cv.get_text() for cv in standings_table.select('.coverages')]
+        for cov in coverages:
+            covs.append(cov[:15])
+
         games = [gm.get_text() for gm in standings_table.select('.row.game')]
 
         for x, game in enumerate(games):
             # content.append(Match(date, leagues[x].text.strip(), kick_off[x], home_team[x], away_team[x], (coverages[x])))
 
             content = {"leagues": leagues[x].text.strip(), "kickoff": kick_off[x], "home_team": home_team[x],
-                       "away_team": away_team[x], "coverages": coverages[x]}
+                       "away_team": away_team[x], "coverages": covs[x]}
 
             scraperwiki.sql.save(['home_team'], data=content)
 
