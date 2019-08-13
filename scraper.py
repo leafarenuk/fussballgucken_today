608184fe9488c8ba403312a670abc62d89f168fe
Rafael Kune <kune@hornetsecurity.com>
2019-08-13 16:21:40 +0200

    first commit

--- scraper.py
+++ scraper.py
@@ -1,24 +1,38 @@
-# This is a template for a Python scraper on morph.io (https://morph.io)
-# including some code snippets below that you should find helpful
+import requests
+from bs4 import BeautifulSoup
+import collections
+import re
+import scraperwiki
 
-# import scraperwiki
-# import lxml.html
-#
-# # Read in a page
-# html = scraperwiki.scrape("http://foo.com")
-#
-# # Find something on the page using css selectors
-# root = lxml.html.fromstring(html)
-# root.cssselect("div[align='left']")
-#
-# # Write out to the sqlite database using scraperwiki library
-# scraperwiki.sqlite.save(unique_keys=['name'], data={"name": "susan", "occupation": "software developer"})
-#
-# # An arbitrary query against the database
-# scraperwiki.sql.select("* from data where 'name'='peter'")
+url = 'https://www.fussballgucken.info/fussball-heute'
 
-# You don't have to do things with the ScraperWiki and lxml libraries.
-# You can use whatever libraries you want: https://morph.io/documentation/python
-# All that matters is that your final data is written to an SQLite database
-# called "data.sqlite" in the current working directory which has at least a table
-# called "data".
+content = []
+Match = collections.namedtuple('Match', 'date league kick_off home away coverages')
+Duell = collections.namedtuple('Duell', 'home away')
+
+
+try:
+    page_response = requests.get(url, timeout=5)
+    
+    if page_response.status_code == 200:
+        page_content = BeautifulSoup(page_response.content, 'lxml')
+        
+        standings_table = page_content.find('div', attrs={'class': 'match-table'})
+        date = [gl.get_text() for gl in standings_table.select('.row.date')]
+        kick_off = [tm.get_text() for tm in standings_table.select('.time')]
+        leagues = standings_table.findAll('a', href=re.compile("wettbewerb"))
+        home_team = [tl.get_text() for tl in standings_table.select('.match .col-xs-5.only-top-padding.text-right')]
+        away_team = [tr.get_text() for tr in standings_table.select('.match .col-xs-5.only-top-padding.text-left')]
+        coverages = [cv.get_text() for cv in standings_table.select('.coverages')]
+        games = [gm.get_text() for gm in standings_table.select('.row.game')]
+        
+        for x, game in enumerate(games):
+            content.append(Match(date, leagues[x].text.strip(), kick_off[x], home_team[x], away_team[x], (coverages[x])))
+            scraperwiki.sql.save(unique_keys=['normal'], data=content)
+
+    else:
+        print(page_response.status_code)
+
+except requests.Timeout as e:
+    print('Timeout occurred for requested page: ' + url)
+    print(str(e))
